504e1cba896d95b834f657196accd72cebcf4ec3
Kyle Brieden <kyle@brieden.org>
2017-11-19 00:06:10 -0500

    Adding configuration variables to config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 GET_IP_URL = 'https://icanhazip.com'
 CF_EMAIL = 'CLOUD_FLARE_EMAIL_ADDRESS'
 CF_TOKEN = 'CLOUD_FLARE_API_KEY'
+RECORDS_TO_WATCH = ['sub.example.com', 'sub.example.net', 'sub2.example.com']
+REPORT_EMAIL = 'me@email.com'
+SMTP_SERVER = 'localhost'
+SMTP_PORT = 25
